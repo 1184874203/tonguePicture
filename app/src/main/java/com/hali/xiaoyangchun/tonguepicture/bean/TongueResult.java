8fa300c12b4085166a23a757d9f4b1a41960de9f
Yangchun Xiao <xiaoych@rd.netease.com>
2019-05-06 19:34:44 +0800

    [update][net]接口细化

--- app/src/main/java/com/hali/xiaoyangchun/tonguepicture/bean/TongueResult.java
+++ app/src/main/java/com/hali/xiaoyangchun/tonguepicture/bean/TongueResult.java
@@ -1,4 +1,40 @@
 package com.hali.xiaoyangchun.tonguepicture.bean;
 
 public class TongueResult {
+    private String key1;
+    private String key2;
+    private String key3;
+    private String key4;
+
+    public String getKey1() {
+        return key1;
+    }
+
+    public void setKey1(String key1) {
+        this.key1 = key1;
+    }
+
+    public String getKey2() {
+        return key2;
+    }
+
+    public void setKey2(String key2) {
+        this.key2 = key2;
+    }
+
+    public String getKey3() {
+        return key3;
+    }
+
+    public void setKey3(String key3) {
+        this.key3 = key3;
+    }
+
+    public String getKey4() {
+        return key4;
+    }
+
+    public void setKey4(String key4) {
+        this.key4 = key4;
+    }
 }
---
+++ app/src/main/java/com/hali/xiaoyangchun/tonguepicture/ui/fragment/TonguePicDetailFragment.java
@@ -0,0 +1,92 @@
+package com.hali.xiaoyangchun.tonguepicture.ui.fragment;
+
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.hali.xiaoyangchun.tonguepicture.R;
+import com.hali.xiaoyangchun.tonguepicture.bean.TongueResult;
+import com.hali.xiaoyangchun.tonguepicture.listener.ChangeListenerManager;
+import com.hali.xiaoyangchun.tonguepicture.listener.ChangeListenr;
+import com.hali.xiaoyangchun.tonguepicture.model.net.CommonRequest;
+import com.hali.xiaoyangchun.tonguepicture.model.net.interfaces.OkGoInterface;
+import com.hali.xiaoyangchun.tonguepicture.ui.base.BaseFragment;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class TonguePicDetailFragment extends BaseFragment implements OkGoInterface, ChangeListenr {
+
+    public static String TonguePicDetailFragment_TAG = "TonguePicDetailFragment_TAG";
+
+    private TextView name, time, otherString, k1, k2, k3, k4, v1, v2, v3, v4;
+    private ImageView tongueImg;
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_tongue_detail;
+    }
+
+    @Override
+    public void initViews() {
+        tongueImg = findView(R.id.iv_tongue);
+        name = findView(R.id.tv_name);
+        time = findView(R.id.tv_time);
+        k1 = findView(R.id.tv_p1_k);
+        k2 = findView(R.id.tv_p2_k);
+        k3 = findView(R.id.tv_p3_k);
+        k4 = findView(R.id.tv_p4_k);
+        v1 = findView(R.id.tv_p1_v);
+        v2 = findView(R.id.tv_p2_v);
+        v3 = findView(R.id.tv_p3_v);
+        v4 = findView(R.id.tv_p4_v);
+        otherString = findView(R.id.tv_otherString);
+    }
+
+    @Override
+    public void initData() {
+        ChangeListenerManager.Companion.getInstance().registerListener("tongue_detail", this);
+        String picPath = mActivity.getIntent().getStringExtra("picPath");
+        CommonRequest.INSTANCE.getTongueDetial(mActivity, picPath, this);
+    }
+
+    @Override
+    public void onSuccess(@Nullable Object response, int requestCode) {
+        Log.i("诊断详情", response.toString());
+        ChangeListenerManager.Companion.getInstance().notifyDataChanged("tongue_detail", response);
+    }
+
+    /**
+     * 张晨昊的解析。。。。。
+     *
+     * @return
+     */
+    private String[] checkResult(String result) {
+        return result.split("-");
+    }
+
+    @Override
+    public void onError(@NotNull String error) {
+
+    }
+
+    @Override
+    public void onChange(@NotNull String key, @NotNull Object data) {
+        if ("tongue_detail".equals(key)) {
+            TongueResult result = new Gson().fromJson((String) data, TongueResult.class);
+            String[] r1 = checkResult(result.getKey1());
+            String[] r2 = checkResult(result.getKey2());
+            String[] r3 = checkResult(result.getKey3());
+            String[] r4 = checkResult(result.getKey4());
+            k1.setText(r1[0] + "");
+            k2.setText(r2[0] + "");
+            k3.setText(r3[0] + "");
+            k4.setText(r4[0] + "");
+            v1.setText(r1[1] + "");
+            v2.setText(r2[1] + "");
+            v3.setText(r3[1] + "");
+            v4.setText(r4[1] + "");
+        }
+    }
+}
